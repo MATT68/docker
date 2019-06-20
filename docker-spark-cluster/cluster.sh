aa5ecbb8a17e79f722eee25646e07b520d1bf4d3
matt68 <matiyoli@gmail.com>
2019-06-20 12:33:15 +0200

    modificado:     db2expc/leeme.txt  	modificado:     docker-spark-cluster/cluster.sh 	modificado:     docker-spark-cluster/spark/Dockerfile 	nuevo archivo:  docker-spark-cluster/spark/config/id_rsa 	nuevo archivo:  docker-spark-cluster/spark/config/id_rsa.pub 	modificado:     hadoop_docker_tensorflow/CreandoUnaImagenDockerHadoop.txt 	modificado:     hadoop_docker_tensorflow/Dockerfile 	modificado:     minikube_cluster_example/Deployment en Kubernetes.txt 	modificado:     minikube_cluster_example/Dockerfile 	modificado:     minikube_cluster_example/app.js 	modificado:     python_app/Dockerfile 	modificado:     python_app/app.py 	modificado:     python_app/docker-compose.yml 	modificado:     python_app/requisitos.txt 	modificado:     python_stack_app/Dockerfile 	modificado:     python_stack_app/app.py 	modificado:     python_stack_app/docker-compose.yml 	modificado:     python_stack_app/requisitos.txt 	modificado:     python_stack_redis_app/app.py 	modificado:     python_stack_redis_app/docker-compose.yml 	modificado:     python_stack_redis_app/requisitos.txt
    	modificado:     hadoop-base (contenido modificado)
    	modificado:     hadoop-cluster (contenido modificado)
    	modificado:     hadoop_pseudocluster_en_docker (contenido modificado)

--- docker-spark-cluster/cluster.sh
+++ docker-spark-cluster/cluster.sh
@@ -25,6 +25,18 @@ function show_info {
   echo "DFS Health @ nodemaster : http://$masterIp:9870/dfshealth.html"
 }
 
+# matt68 add this function
+function show_nodes_info {
+  echo " ### Copy and execute this commands to add the new nodes to your /etc/hosts : "
+  echo ' echo #  >>>>>   Next lines define the hadoop over docker cluster nodes. <<<<<      | sudo tee -a /etc/hosts  '
+  echo ' echo ' `docker inspect -f "{{ .NetworkSettings.Networks.sparknet.IPAddress }}" nodemaster` '"      nodemaster "' '  | sudo tee -a /etc/hosts  '
+  echo ' echo ' `docker inspect -f "{{ .NetworkSettings.Networks.sparknet.IPAddress }}" node2`      '"      node2      "' '   | sudo tee -a /etc/hosts  '
+  echo ' echo ' `docker inspect -f "{{ .NetworkSettings.Networks.sparknet.IPAddress }}" node3`      '"      node3      "' '   | sudo tee -a /etc/hosts  '
+  echo ' echo ' `docker inspect -f "{{ .NetworkSettings.Networks.sparknet.IPAddress }}" node4`      '"      node4      "' '   | sudo tee -a /etc/hosts  '
+  echo ' ### At the end you coud use this command to erase all the ips with the hostnames node* : '
+  echo ' sudo sed '/node/  d' -i /etc/hosts   '  
+}
+
 if [[ $1 = "start" ]]; then
   startServices
   exit
@@ -46,10 +58,10 @@ if [[ $1 = "deploy" ]]; then
 
   # 3 nodes
   echo ">> Starting nodes master and worker nodes ..."
-  docker run -dP --network sparknet --name nodemaster -h nodemaster -it sparkbase
-  docker run -dP --network sparknet --name node2 -it -h node2 sparkbase
-  docker run -dP --network sparknet --name node3 -it -h node3 sparkbase
-  docker run -dP --network sparknet --name node4 -it -h node4 sparkbase
+  docker run -dP --network sparknet --name nodemaster -h nodemaster -it matt68/sparkbase
+  docker run -dP --network sparknet --name node2 -it -h node2 matt68/sparkbase
+  docker run -dP --network sparknet --name node3 -it -h node3 matt68/sparkbase
+  docker run -dP --network sparknet --name node4 -it -h node4 matt68/sparkbase
 
   # Format nodemaster
   echo ">> Formatting hdfs ..."
@@ -63,8 +75,16 @@ if [[ $1 = "info" ]]; then
   exit
 fi
 
+# matt68 add this option
+if [[ $1 = "show_nodes" ]]; then
+  show_nodes_info
+  exit
+fi
+
 echo "Usage: cluster.sh deploy|start|stop"
-echo "                 deploy - create a new Docker network"
-echo "                 start  - start the existing containers"
-echo "                 stop   - stop the running containers" 
-echo "                 info   - useful URLs" 
+echo "                 deploy       - create a new Docker network"
+echo "                 start        - start the existing containers"
+echo "                 stop         - stop the running containers" 
+echo "                 info         - useful URLs" 
+# matt68 add this line : 
+echo "                 show_nodes   - commands to manage the nodes into your /etc/hosts file" 
